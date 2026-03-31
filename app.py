c91c04eac3f5b644932cb402cf1f8cb506ed056f
suhyun <abjkr29@gmail.com>
2026-04-01 08:54:21 +0900

    HTML 파일 보내기

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-#플라스크 필요한거 가져오기
-from flask import Flask
-#플라스크 앱 만들기
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-#'/'주소로 접속하면 실행됨
 @app.route('/')
 def home():
-    return "Hello world!"
+    # 이제는 JSON이 아니라 HTML 파일을 보냅니다
+    return render_template('index.html')
 
-#이 파일을 실행했을때만 나옴
 if __name__ == '__main__':
     app.run(debug=True)
